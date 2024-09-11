7057b620a4eb490e5159aa04b024abd3b199500d
furina-lu <haiyu.lu@lixinchuxing.com>
2024-09-11 15:17:37 +0800

    bug report

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, toRefs } from 'vue'
+
+interface IGroupDetail {
+    created_at: string;
+}
+const props = defineProps<{
+    groupDetail: Partial<IGroupDetail>
+}>()
+
+// const { groupDetail } = toRefs(props) // Comment this code，groupDetail in template will unknown 
+
+</script>
+
+<template>
+  <div>  {{ groupDetail.created_at }}</div>
+   <div>  {{ props.groupDetail.created_at }}</div>
+</template>
